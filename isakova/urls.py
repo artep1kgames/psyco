fec88feba6da9fd6a39bd49bceeb1c64a0c11815
artep1kgames <zadirakoartem1@outlook.com>
2026-07-06 22:08:57 +0300

    Первый день

---
+++ isakova/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home),
+    path('about-me', views.about),
+    path('services', views.services),
+    path('documents', views.documents),
+    path('contacts', views.contacts),
+]
---
+++ isakova/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def documents(request):
+    return render(request, 'documents.html')
+def contacts(request):
+    return render(request, 'contacts.html')
